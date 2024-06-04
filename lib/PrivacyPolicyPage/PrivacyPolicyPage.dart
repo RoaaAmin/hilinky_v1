a8a2679b46e5f82cc0bbcae2c36e30bd54455193
MAHA Al-ATAWI <140245824+imahacs@users.noreply.github.com>
2024-06-04 21:27:55 +0300

    Update PrivacyPolicyPage.dart

--- lib/PrivacyPolicyPage/PrivacyPolicyPage.dart
+++ lib/PrivacyPolicyPage/PrivacyPolicyPage.dart
@@ -1,123 +1,40 @@
 import 'package:flutter/material.dart';
-import 'package:easy_localization/easy_localization.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter_html/flutter_html.dart';
 
-import '../theme/theme_helper.dart';
+class PrivacyPolicyPage extends StatefulWidget {
+  @override
+  _PrivacyPolicyPageState createState() => _PrivacyPolicyPageState();
+}
 
-class PrivacyPolicyPage extends StatelessWidget {
+class _PrivacyPolicyPageState extends State<PrivacyPolicyPage> {
+  String htmlContent = "";
+
+  @override
+  void initState() {
+    super.initState();
+    fetchPrivacyPolicy();
+  }
+
+  Future<void> fetchPrivacyPolicy() async {
+    final response = await http.get('https://sites.google.com/view/hilinky-privacy-policy/%D8%A7%D9%84%D8%B5%D9%81%D8%AD%D8%A9-%D8%A7%D9%84%D8%B1%D8%A6%D9%8A%D8%B3%D9%8A%D8%A9');
+    if (response.statusCode == 200) {
+      setState(() {
+        htmlContent = response.body;
+      });
+    } else {
+      throw Exception('Failed to load privacy policy');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: appTheme.whiteA700,
-      resizeToAvoidBottomInset: false,
       appBar: AppBar(
-        // title: Text(
-        //   context.tr('Privacy Policy'),
-        // ),
+        title: Text('Privacy Policy'),
       ),
       body: SingleChildScrollView(
-        padding: EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text(
-              context.tr('Privacy Policy'),
-              style: GoogleFonts.robotoCondensed(
-                  color: const Color.fromARGB(255, 2, 84, 86),
-                  fontSize: 30, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 10.0),
-            Text(
-              context.tr(
-                  'This Privacy Policy explains how your personal information is collected, used, and disclosed by the Hilinky application.'),
-              style: TextStyle(
-                color: Color(0xFF7EA9BA),
-                fontSize: 12,
-                fontFamily: 'Inter',
-              ),
-            ),
-            SizedBox(height: 20.0),
-            Text(
-              context.tr('Information Collection and Use'),
-              style: TextStyle(
-                color: Color.fromARGB(255, 2, 84, 86),
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                // fontFamily: 'Inter',
-              ),
-            ),
-            SizedBox(height: 10.0),
-            Text(
-              context.tr(
-                  'We may collect some personal information when you use our application. This information is used to provide and improve our services.'),
-              style: TextStyle(
-                color: Color(0xFF7EA9BA),
-                fontSize: 12,
-                fontFamily: 'Inter',
-              ),
-            ),
-            SizedBox(height: 20.0),
-            Text(
-              context.tr('Log Data'),
-              style: TextStyle(
-                color: Color.fromARGB(255, 2, 84, 86),
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                // fontFamily: 'Inter',
-              ),
-            ),
-            SizedBox(height: 10.0),
-            Text(
-              context.tr(
-                  'We may also collect information that your device sends whenever you use our Service.'),
-              style: TextStyle(
-                color: Color(0xFF7EA9BA),
-                fontSize: 12,
-                fontFamily: 'Inter',
-              ),
-            ),
-            SizedBox(height: 20.0),
-            Text(
-              context.tr('Cookies'),
-              style: TextStyle(
-                color: Color.fromARGB(255, 2, 84, 86),
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                // fontFamily: 'Inter',
-              ),
-            ),
-            SizedBox(height: 10.0),
-            Text(
-              context.tr(
-                  'Cookies are files with a small amount of data that are commonly used as anonymous unique identifiers.'),
-              style: TextStyle(
-                color: Color(0xFF7EA9BA),
-                fontSize: 12,
-                fontFamily: 'Inter',
-              ),
-            ),
-            SizedBox(height: 20.0),
-            Text(
-              context.tr('Changes to This Privacy Policy'),
-              style: TextStyle(
-                color: Color.fromARGB(255, 2, 84, 86),
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                // fontFamily: 'Inter',
-              ),
-            ),
-            SizedBox(height: 10.0),
-            Text(
-              context.tr(
-                  'We may update our Privacy Policy from time to time. Thus, you are advised to review this page periodically for any changes.'),
-              style: TextStyle(
-                color: Color(0xFF7EA9BA),
-                fontSize: 12,
-                fontFamily: 'Inter',
-              ),
-            ),
-          ],
-        ),
+        child: Html(data: htmlContent),
       ),
     );
   }
